0c354b81465ba4b325de44c92252a0038b871de2
MACBOOK 86 <macbookprom3max@MACBOOKs-MacBook-Pro.local>
2024-11-13 16:27:38 +0300

    half the css

---
+++ app.js
@@ -0,0 +1,76 @@
+const letterBox = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+]
+const category = {
+  Animals: [
+    'Elephant',
+    'Tiger',
+    'Dolphin',
+    'Kangaroo',
+    'Giraffe',
+    'Zebra',
+    'Lion',
+    'Penguin',
+    'Whale'
+  ],
+  sports: [
+    'Basketball',
+    'Soccer',
+    'Tennis',
+    'Baseball',
+    'Swimming',
+    'Football',
+    'Cricket',
+    'Golf',
+    'Rugby '
+  ],
+  Countries: [
+    'Canada',
+    'Japan',
+    'Brazil',
+    'Australia',
+    'India',
+    'Germany',
+    'France',
+    'Italy',
+    'China'
+  ],
+  foods: [
+    'Pizza',
+    'Sushi',
+    'Burger',
+    'Pasta',
+    'Tacos',
+    'Salad',
+    'Sandwich',
+    'Fries',
+    'steak'
+  ]
+}
+console.log(category)
+console.log(letterBox)
